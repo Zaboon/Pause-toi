bb34a3c3c401dddf6f0fe654c253a54ca33879b7
Pinate_n <nicolas.pinatel@epitech.eu>
2015-09-09 13:51:52 +0200

    second

--- app/src/main/java/com/example/zaboon/pause_toi/MainActivity.java
+++ app/src/main/java/com/example/zaboon/pause_toi/MainActivity.java
@@ -1,43 +1,29 @@
 package com.example.zaboon.pause_toi;
 
-/**
- * Created by Zaboon on 9/8/2015.
- */
-
-import android.content.Intent;
-import android.os.AsyncTask;
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
-import org.json.JSONException;
-import org.json.JSONObject;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by Zaboon on 9/8/2015.
+ */
 
-    protected EditText username;
-    private EditText password;
-    protected String enteredUsername;
-    private final String serverUrl = "path to your server";
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        username = (EditText) findViewById(R.id.username_field);
-        password = (EditText) findViewById(R.id.password_field);
-        Button loginButton = (Button) findViewById(R.id.login);
-        Button registerButton = (Button) findViewById(R.id.register_button);
+        setContentView(R.layout.activity_main);
+        Button          enjoy = (Button) findViewById(R.id.enjoy);
+
+        enjoy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "DETECTION OPERATIONNAL BITCH", Toast.LENGTH_LONG).show();
+            }
+        });
     }
+}
