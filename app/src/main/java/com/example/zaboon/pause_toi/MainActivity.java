2aa8ac9a227e140cc979fa8fdbc1e23164a4868d
Pinate_n <nicolas.pinatel@epitech.eu>
2015-09-10 16:07:16 +0200

    Almost done2

--- app/src/main/java/com/example/zaboon/pause_toi/MainActivity.java
+++ app/src/main/java/com/example/zaboon/pause_toi/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.zaboon.pause_toi;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,8 +30,48 @@ public class MainActivity extends Activity {
         enjoy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(intent);
-                //Toast.makeText(MainActivity.this, "DETECTION OPERATIONNAL BITCH", Toast.LENGTH_LONG).show();
+                AudioManager am = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+                switch (am.getRingerMode()) {
+                    case AudioManager.RINGER_MODE_SILENT:
+                        startActivity(intent);
+                        break;
+                    case AudioManager.RINGER_MODE_VIBRATE:
+                        new AlertDialog.Builder(MainActivity.this)
+                                .setTitle("WARNING !")
+                                .setMessage("Did you desactivate notifications ?")
+                                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        startActivity(new Intent(android.provider.Settings.ACTION_SOUND_SETTINGS));
+                                    }
+                                })
+                                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        startActivity(new Intent(android.provider.Settings.ACTION_SOUND_SETTINGS));
+                                    }
+                                })
+                                .setIcon(android.R.drawable.ic_dialog_alert)
+                                .show();
+                        Log.i("MyApp", "Vibrate mode");
+                        break;
+                    case AudioManager.RINGER_MODE_NORMAL:
+                        new AlertDialog.Builder(MainActivity.this)
+                                .setTitle("WARNING !")
+                                .setMessage("Did you desactivate notifications ?")
+                                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        startActivity(new Intent(android.provider.Settings.ACTION_SOUND_SETTINGS));
+                                    }
+                                })
+                                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        startActivity(new Intent(Settings.ACTION_SOUND_SETTINGS));
+                                    }
+                                })
+                                .setIcon(android.R.drawable.ic_dialog_alert)
+                                .show();
+                        Log.i("MyApp","Normal mode");
+                        break;
+                }
             }
         });
     }
--- app/src/main/java/com/example/zaboon/pause_toi/MapsActivity.java
+++ app/src/main/java/com/example/zaboon/pause_toi/MapsActivity.java
@@ -1,21 +1,183 @@
 package com.example.zaboon.pause_toi;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.cache.memory.impl.FIFOLimitedMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Hashtable;
 
 public class MapsActivity extends FragmentActivity {
-
-    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private GoogleMap googleMap;
+    private final LatLng EPITECH = new LatLng(43.311098, 5.369033);
+    private final LatLng KIEL = new LatLng(43.311098, 5.369033);
+    private Marker marker;
+    private Hashtable<String, String> markers;
+    private ImageLoader imageLoader;
+    private DisplayImageOptions options;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps2);
+        setContentView(R.layout.activity_maps);
+
+        googleMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+
+        initImageLoader();
+        markers = new Hashtable<String, String>();
+        imageLoader = ImageLoader.getInstance();
+
+        options = new DisplayImageOptions.Builder()
+                .showStubImage(R.drawable.tree)        //    Display Stub Image
+                .showImageForEmptyUri(R.drawable.tree)    //    If Empty image found
+                .cacheInMemory()
+                .cacheOnDisc().bitmapConfig(Bitmap.Config.RGB_565).build();
+
+        if (googleMap != null) {
+
+            googleMap.setInfoWindowAdapter(new CustomInfoWindowAdapter());
+
+/*            final Marker hamburg = googleMap.addMarker(new MarkerOptions().position(EPITECH)
+                    .title("Epitech"));
+            markers.put(hamburg.getId(), "https://wallpaperscraft.com/image/nature_park_beautiful_landscape_84256_2560x1080.jpg");*/
+
+            final Marker kiel = googleMap.addMarker(new MarkerOptions().position(KIEL).title("Epitech").snippet("Epitech is not cool").icon(BitmapDescriptorFactory.fromResource(R.drawable.tree)));
+            markers.put(kiel.getId(), "http://www.yodot.com/images/jpeg-images-sm.png");
+
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(EPITECH, 15));
+            googleMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+            googleMap.setMyLocationEnabled(true);
+        }
+    }
+
+    private class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
+
+        private View view;
+
+        public CustomInfoWindowAdapter() {
+            view = getLayoutInflater().inflate(R.layout.custom_info_window,
+                    null);
+        }
+
+        @Override
+        public View getInfoContents(Marker marker) {
+
+            if (MapsActivity.this.marker != null
+                    && MapsActivity.this.marker.isInfoWindowShown()) {
+                MapsActivity.this.marker.hideInfoWindow();
+                MapsActivity.this.marker.showInfoWindow();
+            }
+            return null;
+        }
+
+        @Override
+        public View getInfoWindow(final Marker marker) {
+            MapsActivity.this.marker = marker;
+
+            String url = null;
+
+            if (marker.getId() != null && markers != null && markers.size() > 0) {
+                if (markers.get(marker.getId()) != null &&
+                        markers.get(marker.getId()) != null) {
+                    url = markers.get(marker.getId());
+                }
+            }
+            final ImageView image = ((ImageView) view.findViewById(R.id.badge));
+
+            if (url != null && !url.equalsIgnoreCase("null")
+                    && !url.equalsIgnoreCase("")) {
+                imageLoader.displayImage(url, image, options,
+                        new SimpleImageLoadingListener() {
+                            @Override
+                            public void onLoadingComplete(String imageUri,
+                                                          View view, Bitmap loadedImage) {
+                                super.onLoadingComplete(imageUri, view,
+                                        loadedImage);
+                                getInfoContents(marker);
+                            }
+                        });
+            } else {
+                image.setImageResource(R.drawable.tree);
+            }
+
+            final String title = marker.getTitle();
+            final TextView titleUi = ((TextView) view.findViewById(R.id.title));
+            if (title != null) {
+                titleUi.setText(title);
+            } else {
+                titleUi.setText("");
+            }
+
+            final String snippet = marker.getSnippet();
+            final TextView snippetUi = ((TextView) view
+                    .findViewById(R.id.snippet));
+            if (snippet != null) {
+                snippetUi.setText(snippet);
+            } else {
+                snippetUi.setText("");
+            }
+
+            return view;
+        }
+    }
+
+    private void initImageLoader() {
+        int memoryCacheSize;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
+            int memClass = ((ActivityManager)
+                    getSystemService(Context.ACTIVITY_SERVICE))
+                    .getMemoryClass();
+            memoryCacheSize = (memClass / 8) * 1024 * 1024;
+        } else {
+            memoryCacheSize = 2 * 1024 * 1024;
+        }
+
+        final ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
+                this).threadPoolSize(5)
+                .threadPriority(Thread.NORM_PRIORITY - 2)
+                .memoryCacheSize(memoryCacheSize)
+                .memoryCache(new FIFOLimitedMemoryCache(memoryCacheSize - 1000000))
+                .denyCacheImageMultipleSizesInMemory()
+                .discCacheFileNameGenerator(new Md5FileNameGenerator())
+                .tasksProcessingOrder(QueueProcessingType.LIFO)
+                .build();
+
+        ImageLoader.getInstance().init(config);
+    }
+}
+/*
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private HttpURLConnection connection;
+    private URL url;
+    private Marker marker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
     }
 
@@ -40,6 +202,7 @@ public class MapsActivity extends FragmentActivity {
     }
 
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        marker = mMap.addMarker(new MarkerOptions().position(new LatLng(43.311098, 5.369033)).title("Marker").icon(BitmapDescriptorFactory.fromResource(R.drawable.tree)).title("Parc de la jouvence").snippet("TOTO"));
     }
 }
+*/
